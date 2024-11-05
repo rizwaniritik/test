4f83c466e2886fee8693eb1dfd193d644b0b98f8
ritikr633 <ritik.r@turing.com>
2024-11-05 13:13:03 +0530

    k

--- git.sh
+++ git.sh
@@ -1,7 +1,3 @@
-git config --global pull.rebase true  # Set rebase as the default for all repositories
-git config pull.rebase false         # Set merge as the default for this repository
-
-git pull                # Use the configured strategy
-git pull --rebase       # Use rebase for this pull
-git pull --no-rebase    # Use merge for this pull
-git pull --ff-only     # Use fast-forward only for this pull
+git pull --rebase
+git add .
+git rebase --continue
