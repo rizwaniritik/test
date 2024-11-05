801449dd1103e37f19cd48288219b65453da973e
ritikr633 <ritik.r@turing.com>
2024-11-05 13:11:49 +0530

    k

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
-git branch -a
-git fetch origin
-git push origin --delete test1
-# git push origin :test1
-# git fetch origin  
-# git push origin --delete master/test1
-# git push origin :master/test1
+git config --global pull.rebase true  # Set rebase as the default for all repositories
+git config pull.rebase false         # Set merge as the default for this repository
+
+git pull                # Use the configured strategy
+git pull --rebase       # Use rebase for this pull
+git pull --no-rebase    # Use merge for this pull
+git pull --ff-only     # Use fast-forward only for this pull
